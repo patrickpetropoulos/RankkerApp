4f418dd0e6235e8ee152ed2334a8c49a803b1abb
Patrick <patrickpetropoulos@gmail.com>
2020-06-23 20:36:47 -0400

    Upgraded to NetCore 3.1

--- API/RankkerAPI/Controllers/ValueController.cs
+++ API/RankkerAPI/Controllers/ValueController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace RankkerAPI.Controllers
 {
@@ -7,9 +8,18 @@ namespace RankkerAPI.Controllers
     [ApiController]
     public class ValueController : Controller
     {
+        private ILogger<ValueController> _logger;
+
+        public ValueController(ILogger<ValueController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
         public async Task<IActionResult> Index()
         {
+            // Raises an error event:
+            _logger.LogError("Index was called.");
             return new OkObjectResult("Call Successful");
         }
     }
